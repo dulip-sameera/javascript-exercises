4a9c807b4925f878cf6f408690a0b0d8e0379e62
DS Weliwaththa <dsgitrepo101@gmail.com>
2022-08-03 10:34:08 +0530

    Done repeat string exercise: All tets passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (word, repeat) {
+  let repeatedWord = "";
 
+  if (repeat >= 0) {
+    for (let i = 0; i < repeat; i++) {
+      repeatedWord += word;
+    }
+  } else {
+    repeatedWord = "ERROR";
+  }
+
+  return repeatedWord;
 };
 
 // Do not edit below this line
