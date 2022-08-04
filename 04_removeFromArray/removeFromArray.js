12dde93097920a0043da5991381e049d0b3e3669
DS Weliwaththa <dsgitrepo101@gmail.com>
2022-08-04 09:41:38 +0530

    Redone the removeFromArray Function: All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,18 @@
-const removeFromArray = function (array, ...deleteValues) {
-  const afterDeletion = [];
+// const removeFromArray = function (array, ...deleteValues) {
+//   const afterDeletion = [];
 
-  for (let i = 0; i <= array.length - 1; i++) {
-    if (!deleteValues.includes(array[i])) {
-      afterDeletion.push(array[i]);
-    }
-  }
+//   for (let i = 0; i <= array.length - 1; i++) {
+//     if (!deleteValues.includes(array[i])) {
+//       afterDeletion.push(array[i]);
+//     }
+//   }
 
-  return afterDeletion;
+//   return afterDeletion;
+// };
+
+const removeFromArray = function (...args) {
+  const array = args[0];
+  return array.filter((value) => !args.includes(value));
 };
 
 // Do not edit below this line
