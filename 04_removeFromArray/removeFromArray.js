62d807543f70efe42d4c2167f677135cd9e2d2b4
DS Weliwaththa <dsgitrepo101@gmail.com>
2022-08-04 09:32:50 +0530

    Done removeFormArray Exercise : All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...deleteValues) {
+  const afterDeletion = [];
 
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (!deleteValues.includes(array[i])) {
+      afterDeletion.push(array[i]);
+    }
+  }
+
+  return afterDeletion;
 };
 
 // Do not edit below this line
