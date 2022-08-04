cec3ca547ad53a563803606b0623af6ebb642100
DS Weliwaththa <dsgitrepo101@gmail.com>
2022-08-04 10:38:37 +0530

    Done sumAll exercise : All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (startValue, endValue) {
+  let total = 0;
 
+  if (
+    typeof startValue !== "number" ||
+    typeof endValue !== "number" ||
+    startValue < 0 ||
+    endValue < 0
+  ) {
+    return "ERROR";
+  }
+
+  let start = startValue > endValue ? endValue : startValue;
+  let end = startValue > endValue ? startValue : endValue;
+
+  for (let i = start; i <= end; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
