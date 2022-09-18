303f49bf7599c9d93ff23b3bcce7becc6c90e04c
dulip-sameera <dsgitrepo101@gmail.com>
2022-09-18 10:16:10 +0530

    create a function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce(
+    function (oldestPerson, person) {
+      if (!("yearOfDeath" in person)) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
 
+      if (
+        person.yearOfDeath - person.yearOfBirth >
+        oldestPerson.yearOfDeath - oldestPerson.yearOfBirth
+      ) {
+        oldestPerson.name = person.name;
+        oldestPerson.yearOfBirth = person.yearOfBirth;
+        oldestPerson.yearOfDeath = person.yearOfDeath;
+      }
+
+      return oldestPerson;
+    },
+    {
+      name: "",
+      yearOfBirth: 0,
+      yearOfDeath: 0,
+    }
+  );
 };
 
 // Do not edit below this line
