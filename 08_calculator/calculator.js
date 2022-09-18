7d4361e245a0826a5782d887b41d2253b9e6671b
dulip-sameera <dsgitrepo101@gmail.com>
2022-09-18 07:38:58 +0530

    Done sum, multiply and power calculations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,16 +1,29 @@
-const add = function (a, b) {
+const add = function (a = 0, b = 0) {
   return a + b;
 };
 
-const subtract = function (a, b) {
+const subtract = function (a = 0, b = 0) {
   return a - b;
 };
 
-const sum = function () {};
+const sum = function (numArray = []) {
+  let tot = 0;
+  numArray.forEach(function (value) {
+    tot += value;
+  });
+  return tot;
+};
 
-const multiply = function () {};
-
-const power = function () {};
+const multiply = function (numArray = []) {
+  let tot = 1;
+  numArray.forEach(function (value) {
+    tot *= value;
+  });
+  return tot;
+};
+const power = function (number, power) {
+  return number ** power;
+};
 
 const factorial = function () {};
 
