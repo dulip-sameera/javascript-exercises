240681ebdbdec4e91a463d1871343d6a8e3d5cf3
dulip-sameera <dsgitrepo101@gmail.com>
2022-09-18 09:01:51 +0530

    Done function to check wether a word is  a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,41 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const punctuationSymbols = [
+    ".",
+    "?",
+    "!",
+    ",",
+    ":",
+    ";",
+    "-",
+    "[",
+    "]",
+    "{",
+    "}",
+    "(",
+    ")",
+    "'",
+    '"',
+    " ",
+  ];
 
+  let wordArray = word.split("");
+
+  // removes all the punctuations and replace them with ""
+  for (let i = 0; i < wordArray.length; i++) {
+    for (let j = 0; j < punctuationSymbols.length; j++) {
+      if (wordArray[i] === punctuationSymbols[j]) {
+        wordArray[i] = "";
+      }
+    }
+  }
+  // had to join the array to remove all "" replaced by the above code
+  word = wordArray.join("").toLowerCase();
+  reversedWord = wordArray.reverse().join("").toLowerCase();
+
+  if (word != reversedWord) {
+    return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
