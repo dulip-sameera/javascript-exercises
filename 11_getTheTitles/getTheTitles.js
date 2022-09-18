84ae0c750d49cc0800d579da269d1b113254943b
dulip-sameera <dsgitrepo101@gmail.com>
2022-09-18 09:36:55 +0530

    Create a function to ge the titles from a list of books

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  let bookTitles = books.reduce(function (titles, book) {
+    titles.push(book.title);
+    return titles;
+  }, []);
 
+  return bookTitles;
 };
 
 // Do not edit below this line
